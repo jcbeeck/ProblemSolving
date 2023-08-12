4de20d05d0d2b3d9eaa01a66be3ad5bed383380e
Jan Beeck <jcbeeck@gmail.com>
2023-08-12 14:03:07 -0500

    Commit - LeetCode200
    200. Number of Islands
    Given an m x n 2D binary grid grid which represents a map of '1's (land) and '0's (water), return the number of islands.
    An island is surrounded by water and is formed by connecting adjacent lands horizontally or vertically. You may assume all four edges of the grid are all surrounded by water.

---
+++ LeetCode200.php
@@ -0,0 +1,99 @@
+<?php
+
+    class Solution {
+
+        /**
+         * @param String[][] $grid
+         * @return Integer
+         */
+
+        public $y;
+
+        function numIslands($grid)
+        {
+            
+            //Create a mirror of the grid + frame
+            for($i = 0; $i < count($grid) + 2; $i++) //rows
+            {
+                for($j = 0; $j < count($grid[0]) + 2; $j++) //cols
+                {
+                    $mirror[$i][$j] = 0;    
+                    
+                }
+            }
+
+            //print_r($mirror); 
+
+            for($i = 0; $i < count($grid); $i++) //rows
+            {
+                for($j = 0; $j < count($grid[0]); $j++) //cols
+                {
+                    $mirror[$i+1][$j+1] = $grid[$i][$j];    
+                    
+                }
+            }
+
+            //print_r($mirror); 
+
+            $island = 0;
+
+            for($x = 1; $x <= 5; $x++) //rows
+            {
+                for($this->y = 1; $this->y <= 5; $this->y = $this->y + 2) //cols
+                {
+                    if ($this->y == 6){break;}
+
+                    //criteria 1
+                    if($mirror[$x+1][$this->y-1] == 1)
+                    {
+                        $island = $island + 1;
+                    }
+                    
+                    //criteria 2
+                    if($mirror[$x][$this->y] == 1)
+                    {
+                        $island = $island + 1;
+                    }            
+                }
+                
+            }//end 2-for
+
+
+            if($this->y >= 5)
+            {
+                // smaller table
+                $ans = intdiv($island, $this->y);
+            }
+            else
+            {
+                $ans = intdiv($island, 2);
+            }
+
+            return $ans;
+            
+        } // end f()
+
+    }//end class
+
+    $island = new Solution();
+
+    $grid = [
+            ["1","1","1","1","0"],
+            ["1","1","0","1","0"],
+            ["1","1","0","0","0"],
+            ["0","0","0","0","0"]];
+  
+           /* $grid = [
+                ["1","1","0","0","0"],
+                ["1","1","0","0","0"],
+                ["0","0","1","0","0"],
+                ["0","0","0","1","1"]
+            ]; */
+  
+
+    $res = $island->numIslands($grid);
+
+    echo $res;
+   
+
+?>
